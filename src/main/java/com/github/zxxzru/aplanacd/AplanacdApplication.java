22f320343d17a72466e4fb3734d0f1f38faf7d74
zxxz <viacheslav.modestov@gmail.com>
2019-01-03 16:41:08 +0300

    Initial

---
+++ src/main/java/com/github/zxxzru/aplanacd/AplanacdApplication.java
@@ -0,0 +1,14 @@
+package com.github.zxxzru.aplanacd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplanacdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplanacdApplication.class, args);
+	}
+
+}
+
