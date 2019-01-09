b62ad5a4d9b9e5e961f729f2065307e03f10d32c
zxxz <viacheslav.modestov@gmail.com>
2019-01-09 12:49:20 +0300

    change post disk, add default user to start up

--- src/main/java/com/github/zxxzru/aplanacd/LoadData.java
+++ src/main/java/com/github/zxxzru/aplanacd/LoadData.java
@@ -18,34 +18,35 @@ import java.util.*;
 @Slf4j
 class LoadData {
 
-    User u1 = new User("user-1-name", "user-1-address", "user-1-login", "user-1-password");
+    User defaultUser = new User("default", "address", "login", "no-password");
     User u2 = new User("user-2-name", "user-2-address", "user-2-login", "user-2-password");
     User u3 = new User("user-3-name", "user-3-address", "user-3-login", "user-3-password");
     User u4 = new User("user-4-name", "user-4-address", "user-4-login", "user-4-password");
     User u5 = new User("user-5-name", "user-5-address", "user-5-login", "user-5-password");
-    User[] users = {u1, u2, u3, u4, u5};
+    User u6 = new User("user-6-name", "user-6-address", "user-6-login", "user-6-password");
+    User[] users = {defaultUser, u2, u3, u4, u5, u6};
 
     Disk[] disks = {
-            new Disk("disk-1-name", "disk-1-company", 1, u1),
-            new Disk("disk-2-name", "disk-2-company", 2, u2),
-            new Disk("disk-3-name", "disk-3-company", 3, u3),
-            new Disk("disk-4-name", "disk-4-company", 4, u4),
-            new Disk("disk-5-name", "disk-5-company", 5, u5),
-            new Disk("disk-6-name", "disk-6-company", 6, u1),
-            new Disk("disk-7-name", "disk-7-company", 7, u1),
-            new Disk("disk-8-name", "disk-8-company", 8, u3),
-            new Disk("disk-9-name", "disk-9-company", 9, u1),
-            new Disk("disk-10-name", "disk-10-company", 10, u4),
-            new Disk("disk-11-name", "disk-11-company", 11, u5),
-            new Disk("disk-12-name", "disk-12-company", 12, u2),
-            new Disk("disk-13-name", "disk-13-company", 13, u3),
-            new Disk("disk-14-name", "disk-14-company", 14, u4),
-            new Disk("disk-15-name", "disk-15-company", 15, u4),
-            new Disk("disk-16-name", "disk-16-company", 16, u1),
-            new Disk("disk-17-name", "disk-17-company", 17, u3),
-            new Disk("disk-18-name", "disk-18-company", 18, u1),
-            new Disk("disk-19-name", "disk-19-company", 19, u3),
-            new Disk("disk-20-name", "disk-20-company", 20, u2),
+            new Disk("disk-1-name", "disk-1-company", 1, u2),
+            new Disk("disk-2-name", "disk-2-company", 2, u3),
+            new Disk("disk-3-name", "disk-3-company", 3, u4),
+            new Disk("disk-4-name", "disk-4-company", 4, u5),
+            new Disk("disk-5-name", "disk-5-company", 5, u6),
+            new Disk("disk-6-name", "disk-6-company", 6, u2),
+            new Disk("disk-7-name", "disk-7-company", 7, u2),
+            new Disk("disk-8-name", "disk-8-company", 8, u4),
+            new Disk("disk-9-name", "disk-9-company", 9, u2),
+            new Disk("disk-10-name", "disk-10-company", 10, u5),
+            new Disk("disk-11-name", "disk-11-company", 11, u6),
+            new Disk("disk-12-name", "disk-12-company", 12, u3),
+            new Disk("disk-13-name", "disk-13-company", 13, u4),
+            new Disk("disk-14-name", "disk-14-company", 14, u5),
+            new Disk("disk-15-name", "disk-15-company", 15, u5),
+            new Disk("disk-16-name", "disk-16-company", 16, u2),
+            new Disk("disk-17-name", "disk-17-company", 17, u4),
+            new Disk("disk-18-name", "disk-18-company", 18, u2),
+            new Disk("disk-19-name", "disk-19-company", 19, u4),
+            new Disk("disk-20-name", "disk-20-company", 20, u3),
     };
 
     private String makeName(String n, String pattern){
--- src/main/java/com/github/zxxzru/aplanacd/disk/DiskController.java
+++ src/main/java/com/github/zxxzru/aplanacd/disk/DiskController.java
@@ -3,8 +3,10 @@ package com.github.zxxzru.aplanacd.disk;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.github.zxxzru.aplanacd.user.User;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,15 +20,22 @@ import org.springframework.hateoas.Resources;
 
 import org.springframework.http.ResponseEntity;
 
+import com.github.zxxzru.aplanacd.user.UserRepository;
+
 @RestController
 class DiskController {
 
     private final DiskRepository repository;
     private final DiskResourceAssembler assembler;
+    private final UserRepository userRepository;
 
-    DiskController(DiskRepository repository, DiskResourceAssembler assembler) {
+    DiskController(DiskRepository repository,
+                   UserRepository userRepository,
+                   DiskResourceAssembler assembler
+                   ) {
         this.repository = repository;
         this.assembler = assembler;
+        this.userRepository = userRepository;
     }
 
     // Aggregate root
@@ -40,8 +49,20 @@ class DiskController {
         return new Resources<>(disks, assembler.allToResource());
     }
 
+    private final User getUserById(Long id){
+        Optional<User> userOptional = userRepository.findById(id);
+        User user = userOptional.isPresent() ? userOptional.get() :
+                userRepository.findById(new Long(1)).get();
+                return user;
+    }
+
     @PostMapping("/disk")
    ResponseEntity<?> newDisk(@RequestBody Disk newDisk) throws URISyntaxException {
+        Long userId = newDisk.getUser().getId();
+        User user = getUserById(userId);
+        // TODO: What to do when Optional has nothing?
+        // right now  fake User with ID: 1 is created at start up.
+        newDisk.setUser(user);
         Resource<Disk> resource = assembler.toResource(repository.save(newDisk));
 
         return ResponseEntity
@@ -61,8 +82,12 @@ class DiskController {
 
     @PutMapping("/disk/{id}")
     ResponseEntity<?>replaceDisk(@RequestBody Disk newDisk, @PathVariable Long id) throws URISyntaxException {
-
+        Long userId = newDisk.getUser().getId();
+        User user = getUserById(userId);
+        // reset user in case it was not set before
+        newDisk.setUser(user);
         Disk dsk = repository.findById(id)
+                // TODO: values below are not necessarily not null
                 .map(disk -> {
                     disk.setName(newDisk.getName());
                     disk.setCompany(newDisk.getCompany());
